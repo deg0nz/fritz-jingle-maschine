1ec7b90a72504565146d75e9f480237261c02d41
Beh <b@kayuk.de>
2020-10-19 21:41:19 +0200

    Get rid of everything in the downloader we don't need there

--- fritz-jingle-downloader/src/cli.rs
+++ fritz-jingle-downloader/src/cli.rs
@@ -1,8 +1,7 @@
-use clap::{App, Arg, ArgMatches};
 use std::path::Path;
-use super::maschine::Maschine;
 use super::downloader::Downloader;
 use eyre::Result;
+use clap::{App, Arg, ArgMatches};
 use futures::poll;
 
 pub struct Cli <'a> {
@@ -16,36 +15,11 @@ impl<'a> Cli <'a> {
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .arg(
-            Arg::new("RUN")
-                .takes_value(false)
-                .about("Runs the main app")
-                .short('r')
-                .long("run")
-                .requires_all(&["FILES-PATH", "BUTTON-PIN"])
-        )
-        .arg(
-             Arg::new("FILES-PATH")
-                 .about("Path to the Jingles files containing db.json and a folder called files containing the MP3s.")
-                 .takes_value(true)
-                 .short('f')
-                 .long("files-path")
-                 .default_value(".")
-        )
-        .arg(
-             Arg::new("BUTTON-PIN")
-             .about("Specifies the Raspberry Pi GPIO pin for the trigger button. BCM numbering is used.")
-             .takes_value(true)
-             .short('p')
-             .long("pin")
-             .required_unless_present("DOWNLOAD")
-            )
-        .arg(
-            Arg::new("DOWNLOAD")
+            Arg::new("FILES-PATH")
             .about("Downloads or updates all the jingles from Fritz to a given path. If a db.json is found in the path, an update is assumed.")
-            .short('d')
-            .long("download-jingles")
-            .required_unless_present_any(&["RUN", "BUTTON-PIN"])
-            .requires("FILES-PATH")
+            .short('f')
+            .long("files-path")
+            .required(true)
         );
 
         Self {
@@ -53,39 +27,15 @@ impl<'a> Cli <'a> {
         }
     }
 
-    pub fn process_arguments(&self) {
+    pub async fn process_arguments(&self) {
         // TODO: There has to be another solution to this than cloning?!
         let app = self.app.clone();
         let matches = app.get_matches().clone();
          
-        if matches.is_present("RUN") {
-            self.run_maschine(matches);
-        } else if matches.is_present("DOWNLOAD") {
-            self.run_download(matches);
-        }
+        self.run_download(matches).await?;
     }
 
-    fn run_maschine(&self, matches: ArgMatches) {
-        let jingles_path;
-        let button_pin;
-
-        if let Some(files_path) = matches.value_of("FILES-PATH") {
-            jingles_path = Path::new(files_path).to_path_buf();
-        } else {
-            panic!();
-        }
-
-        if let Some(pin) = matches.value_of("BUTTON-PIN") {
-            button_pin = pin.parse::<u64>().unwrap();
-        } else {
-            panic!();
-        }
-
-        let mut jingles_maschine = Maschine::new(button_pin, jingles_path);
-        jingles_maschine.run().unwrap();
-    }
-
-    fn run_download(&self, matches: ArgMatches) {
+    async fn run_download(&self, matches: ArgMatches) {
         let jingles_path;
 
         if let Some(files_path) = matches.value_of("FILES-PATH") {
@@ -93,9 +43,8 @@ impl<'a> Cli <'a> {
         } else {
             panic!();
         }
-
         
-        //  block_on(Downloader::new().await);
-        //     downloader.run();
+        let downloader = Downloader::new().await?;
+        downloader.run();
     }
 }
--- fritz-jingle-downloader/src/main.rs
+++ fritz-jingle-downloader/src/main.rs
@@ -1,6 +1,13 @@
 mod cli;
 mod downloader;
 
-fn main() {
-    println!("Hello, world!");
+use cli::Cli;
+use futures::prelude::*;
+use tokio::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let cli = Cli::new();
+    rt.block_on(cli.process_arguments());
 }
