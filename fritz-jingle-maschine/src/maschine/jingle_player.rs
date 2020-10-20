696792571096d010d15b914c5c72a2585ac03c98
Beh <b@kayuk.de>
2020-10-21 01:29:36 +0200

    rename get random jingle func

--- fritz-jingle-maschine/src/maschine/jingle_player.rs
+++ fritz-jingle-maschine/src/maschine/jingle_player.rs
@@ -13,7 +13,8 @@ impl JinglePlayer {
     pub fn new(jingles_base_path: PathBuf) -> Self {
         let device = rodio::default_output_device().unwrap();
         let db_path = jingles_base_path.join("db.json");
-        let jingles_db = JinglesDb::new(db_path).unwrap();
+        let mut jingles_db = JinglesDb::new(db_path).unwrap();
+        jingles_db.load().expect("Error loading Database");
 
         Self {
             device,
@@ -31,7 +32,7 @@ impl JinglePlayer {
     }
 
     pub fn play_random(&self) -> Result<()>{
-        let jingle = self.jingles_db.get_random_entry();
+        let jingle = self.jingles_db.get_random_jingle();
         self.print_jingle_playing(&jingle);
         let jingle_file_path = self.jingles_base_path.join(jingle.file_path);
         println!("Jingle file path: {}", jingle_file_path.to_str().unwrap());
