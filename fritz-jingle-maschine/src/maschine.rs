99ebecc45e4c5d9fb3493afd389cb1a22e57f0ff
Beh <b@kayuk.de>
2020-10-24 01:10:43 +0200

    Fix broken maschine loop

--- fritz-jingle-maschine/src/maschine.rs
+++ fritz-jingle-maschine/src/maschine.rs
@@ -1,7 +1,7 @@
 mod jingle_player;
 
 use jingle_player::JinglePlayer;
-use sysfs_gpio::{Direction, Pin};
+use sysfs_gpio::{Direction, Edge, Pin};
 use std::path::PathBuf;
 use eyre::Result;
 
@@ -24,24 +24,24 @@ impl Maschine {
     pub fn run(&mut self) -> Result<()>{
         self.button.with_exported(|| {
             self.button.set_direction(Direction::In)?;
-    
+            self.button.set_edge(Edge::FallingEdge)?;
             let mut btn_poller = self.button.get_poller()?;
             let mut count = 0;
             println!("Ready");
             loop {
                 let btn_value = btn_poller.poll(5)?;
                 if let Some(val ) = btn_value {
-                    dbg!("Some btn_value");
+                    
                     if val == 0 {
                         println!("{}: Got 0", count);
+
+                        if let Err(e) = self.player.play_random() {
+                            dbg!(e);
+                        };
                     }
-                    // println!("{} Got button press on pin: {}", count, val);
-                    if let Err(e) = self.player.play_random() {
-                        dbg!(e);
-                    };
+
                     count += 1;
                 }
-                // sleep(Duration::from_millis(20));
             }
         })?;
 
