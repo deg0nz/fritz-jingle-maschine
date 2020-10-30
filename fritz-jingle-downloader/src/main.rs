95203b4bd0dbaf7f7087c5aa897c90827510a4d7
Beh <b@kayuk.de>
2020-10-30 23:01:41 +0100

    Use tokio macro in main of downloader

--- fritz-jingle-downloader/src/main.rs
+++ fritz-jingle-downloader/src/main.rs
@@ -2,13 +2,17 @@ mod cli;
 mod downloader;
 
 use cli::Cli;
+use eyre::Error;
+use futures::executor::block_on;
 
-// TODO: use tokio macro to automatically setup runtine as in https://github.com/benkay86/async-applied/blob/master/reqwest-tokio/src/main.rs
-fn main() {
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // let mut rt = tokio::runtime::Runtime::new().unwrap();
     let cli = Cli::new();
-    match rt.block_on(cli.process_arguments()) {
+    match block_on(cli.process_arguments()) {
         Ok(_) => println!("Done"),
         Err(e) => println!("An error ocurred: {}", e),
     };
+
+    Ok(())
 }
